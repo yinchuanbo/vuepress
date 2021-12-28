ca4c0aaf23a538a4e97899f1328d777bf0530b04
YinHao <yinhaocomeon@gmail.com>
2021-12-28 16:05:13 +0800

    新建 assets

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -54,7 +54,7 @@ module.exports = {
         // 最后更新时间
         lastUpdated: 'Last Updated',
         // 作者
-        author: 'reco_luan',
+        author: 'YinHAO',
         // 作者头像
         authorAvatar: '/avatar.png',
         // 备案号
